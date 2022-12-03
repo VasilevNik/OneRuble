8aefdc28db760cecb9b30730fc653b5e6ebebcbd
Nikolay Vasilev <lexuspexus@yandex.ru>
2022-12-03 16:22:55 +0300

    Change main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int balance = 530; // стартовый баланс
+        int popolnenie = 1345; // сумма пополнения
+        int fix = 100;
+
+        int bonus;
+        if (popolnenie>=1000) {
+            bonus = (popolnenie/fix);
+        } else {
+            bonus=0;
+        }
+
+        System.out.println("Начисленный бонус - " + bonus + " руб.");
+        System.out.println("Итоговый баланс - " + (balance + popolnenie + bonus) + " руб.");
+
     }
 }
